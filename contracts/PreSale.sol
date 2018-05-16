714b0a1517c7aed6ce7e405bc0978eec75d8928a
Sue Kah Yong <skybach@gmail.com>
2018-05-16 17:14:16 +0800

    remove generated contracts and add ours

---
+++ contracts/PreSale.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.4;
+
+import "../../openzeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
+
+contract PreSale is RefundableCrowdsale {
+  function PreSale() {
+    // constructor
+  }
+}
