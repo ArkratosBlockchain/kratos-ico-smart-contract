b455d77032b4dd9a83d8333668572248453bd0cc
Sue Kah Yong <skybach@gmail.com>
2018-05-22 15:37:40 +0800

    add custom timelock feature

--- contracts/KratosToken.sol
+++ contracts/KratosToken.sol
@@ -8,9 +8,66 @@ contract KratosToken is StandardBurnableToken, PausableToken {
     string public name = "KRATOS";
     string public symbol = "TOS";
     uint8 public decimals = 18;
-    uint256 totalSupply_ = 30000000000000000000000000;
+
+    mapping(address => uint256) timelock;
 
     function KratosToken() {
         // constructor
+    }
+
+    event TimeLocked(address indexed beneficary, uint256 timestamp);
+    event TimeUnlocked(address indexed beneficary);
+
+    /**
+    * @dev Modifier to make a function callable only when the contract is not timelocked or timelock expired.
+    */
+    modifier whenNotTimelocked() {
+        require(timelock[this] == 0 || timelock[this] > block.timestamp);
+        _;
+    }
+
+    /**
+    * @dev Modifier to make a function callable only when the contract is timelocked and not expired.
+    */
+    modifier whenTimelocked() {
+        require(timelock[this] > 0 && timelock[this] < block.timestamp);
+        _;
+    }
+
+    /**
+    * @dev called by the owner to timelock token belonging to beneficary
+    */
+    function addTimelock(address beneficary, uint256 timestamp) onlyOwner whenNotTimelocked public {
+        require(timestamp > 0);
+        timelock[beneficary] = timestamp;
+        emit TimeLocked(beneficary, timestamp);
+    }
+
+    /**
+    * @dev called by the owner to timeunlock token belonging to beneficary
+    */
+    function removeTimelock(address beneficary) onlyOwner whenTimelocked public {
+        timelock[beneficary] = 0;
+        emit TimeUnlocked(beneficary);
+    }
+
+    function transfer(address _to, uint256 _value) public whenNotTimelocked returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotTimelocked returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotTimelocked returns (bool) {
+        return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenNotTimelocked returns (bool success) {
+        return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotTimelocked returns (bool success) {
+        return super.decreaseApproval(_spender, _subtractedValue);
     }
 }
