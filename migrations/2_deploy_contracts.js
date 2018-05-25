1cd5108bc0209d2bb7c84f9c62a11f7366a7635d
Sue Kah Yong <skybach@gmail.com>
2018-05-25 12:11:55 +0800

    update deployment scripts required of new codes and new test cases

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,20 @@
 var KratosToken = artifacts.require("./KratosToken.sol");
 var KratosPresale = artifacts.require("./KratosPresale.sol");
 
-module.exports = function(deployer) {
+module.exports = async (deployer) => {
 
-  const goal = web3.toWei("5000", "ether");
-  const cap = web3.toWei("80000", "ether");
-  const openingTime = web3.eth.getBlock('latest').timestamp;
+  const tokenTotalSupply = 300000000000000000000000000;
+  const tokenPresaleSupply = 80000000000000000000000000;
+  const goal = web3.toWei("1", "ether");
+  const cap = web3.toWei("10", "ether");
+  const openingTime = web3.eth.getBlock('latest').timestamp+5;
   const closingTime = openingTime + 86400 * 20; // 20 days
   const rate = new web3.BigNumber(1250);
-  const wallet = web3.eth.accounts[0];
+  const wallet = web3.eth.accounts[1];
 
-  return deployer.then(() => {
-    return deployer.deploy(KratosToken);
-  }).then(() => {
+  return deployer.then(async () => {
+    return deployer.deploy(KratosToken, tokenTotalSupply);
+  }).then(async () => {
     return deployer.deploy(
       KratosPresale,
       goal,
@@ -22,7 +24,12 @@ module.exports = function(deployer) {
       rate,
       wallet,
       KratosToken.address, 
-      // {gas: web3.eth.getBlock('latest').gasLimit, gasPrice: web3.toWei(0.5, 'gwei')}
     );
+  }).then(async () => {
+    web3.currentProvider.send({jsonrpc: "2.0", method: "evm_increaseTime", params: [10], id: Date.now()});
+
+    const token = KratosToken.at(KratosToken.address);
+    await token.transfer(KratosPresale.address, tokenPresaleSupply);
+    token.enableTimelock(web3.eth.getBlock('latest').timestamp + 86400 * 180);
   });
 };
