06d9e200f5789cb11bb76b3ea2108989b3090ce3
Sue Kah Yong <skybach@gmail.com>
2018-05-25 12:33:14 +0800

    fix timelock

--- contracts/KratosToken.sol
+++ contracts/KratosToken.sol
@@ -9,10 +9,13 @@ contract KratosToken is StandardBurnableToken, PausableToken {
     string public symbol = "TOS";
     uint8 public decimals = 18;
 
-    mapping(address => uint256) timelock;
+    uint256 timelockTimestamp = 0;
+    mapping(address => uint256) public timelock;
 
-    function KratosToken() {
+    function KratosToken(uint256 totalSupply) {
         // constructor
+        totalSupply_ = totalSupply;
+        balances[msg.sender] = totalSupply;
     }
 
     event TimeLocked(address indexed beneficary, uint256 timestamp);
@@ -21,24 +24,37 @@ contract KratosToken is StandardBurnableToken, PausableToken {
     /**
     * @dev Modifier to make a function callable only when the contract is not timelocked or timelock expired.
     */
-    modifier whenNotTimelocked() {
-        require(timelock[this] == 0 || timelock[this] > block.timestamp);
+    modifier whenNotTimelocked(address beneficary) {
+        require(timelock[beneficary] == 0 || timelock[beneficary] <= block.timestamp);
         _;
     }
 
     /**
     * @dev Modifier to make a function callable only when the contract is timelocked and not expired.
     */
-    modifier whenTimelocked() {
-        require(timelock[this] > 0 && timelock[this] < block.timestamp);
+    modifier whenTimelocked(address beneficary) {
+        require(timelock[beneficary] > 0 && timelock[beneficary] > block.timestamp);
         _;
+    }
+
+    function enableTimelock(uint256 _timelockTimestamp) onlyOwner public {
+        require(timelockTimestamp == 0 || timelockTimestamp > block.timestamp);
+        timelockTimestamp = _timelockTimestamp;
+    }
+
+    function disableTimelock() onlyOwner public {
+        timelockTimestamp = 0;
     }
 
     /**
     * @dev called by the owner to timelock token belonging to beneficary
     */
-    function addTimelock(address beneficary, uint256 timestamp) onlyOwner whenNotTimelocked public {
-        require(timestamp > 0);
+    function addTimelock(address beneficary, uint256 timestamp) public onlyOwner whenNotTimelocked(beneficary) {
+        _addTimelock(beneficary, timestamp);
+    }
+
+    function _addTimelock(address beneficary, uint256 timestamp) internal whenNotTimelocked(beneficary) {
+        require(timestamp > block.timestamp);
         timelock[beneficary] = timestamp;
         emit TimeLocked(beneficary, timestamp);
     }
@@ -46,28 +62,32 @@ contract KratosToken is StandardBurnableToken, PausableToken {
     /**
     * @dev called by the owner to timeunlock token belonging to beneficary
     */
-    function removeTimelock(address beneficary) onlyOwner whenTimelocked public {
+    function removeTimelock(address beneficary) onlyOwner whenTimelocked(beneficary) public {
         timelock[beneficary] = 0;
         emit TimeUnlocked(beneficary);
     }
 
-    function transfer(address _to, uint256 _value) public whenNotTimelocked returns (bool) {
+    function transfer(address _to, uint256 _value) public whenNotTimelocked(msg.sender) returns (bool) {
+        if (timelockTimestamp > block.timestamp)
+            _addTimelock(_to, timelockTimestamp);
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public whenNotTimelocked returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotTimelocked(_from) returns (bool) {
+        if (timelockTimestamp > block.timestamp)
+            _addTimelock(_to, timelockTimestamp);
         return super.transferFrom(_from, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value) public whenNotTimelocked returns (bool) {
+    function approve(address _spender, uint256 _value) public whenNotTimelocked(_spender) returns (bool) {
         return super.approve(_spender, _value);
     }
 
-    function increaseApproval(address _spender, uint _addedValue) public whenNotTimelocked returns (bool success) {
+    function increaseApproval(address _spender, uint _addedValue) public whenNotTimelocked(_spender) returns (bool success) {
         return super.increaseApproval(_spender, _addedValue);
     }
 
-    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotTimelocked returns (bool success) {
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotTimelocked(_spender) returns (bool success) {
         return super.decreaseApproval(_spender, _subtractedValue);
     }
 }
