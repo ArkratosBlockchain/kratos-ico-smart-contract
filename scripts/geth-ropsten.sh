1565522194680534b76c2a300eeefa121a7c3144
Sue Kah Yong <skybach@gmail.com>
2018-05-26 18:10:35 +0800

    rename script

---
+++ scripts/geth-ropsten.sh
@@ -0,0 +1,2 @@
+geth --fast --cache=1048 --testnet --rpc --rpcapi "eth,net,web3,personal" --rpccorsdomain '*' --rpcaddr localhost --rpcport 8545 --unlock 0x107be2ff8b6d5c73950c068ebc8085a743e5044f --password password
+
--- scripts/geth-test.sh
+++
@@ -1,2 +0,0 @@
-geth --fast --cache=1048 --testnet --rpc --rpcapi "eth,net,web3,personal" --rpccorsdomain '*' --rpcaddr localhost --rpcport 8545
-
