50d2607ae3c7a9b485cfdcfc6042ee17cb4580ee
Sue Kah Yong <skybach@gmail.com>
2018-05-26 18:10:53 +0800

    debug deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,15 +7,24 @@ module.exports = async (deployer) => {
   const tokenPresaleSupply = 80000000000000000000000000;
   const goal = web3.toWei("1", "ether");
   const cap = web3.toWei("10", "ether");
-  const openingTime = web3.eth.getBlock('latest').timestamp+5;
+  const openingTime = web3.eth.getBlock('latest').timestamp+1000; // !IMPT :: opening timestamp has to be much later when deploying to public networks as it takes some time before contract gets initialized
   const closingTime = openingTime + 86400 * 20; // 20 days
   const rate = new web3.BigNumber(1250);
   const wallet = web3.eth.accounts[1];
 
+  console.log(goal);
+  console.log(cap);
+  console.log(openingTime);
+  console.log(closingTime);
+  console.log(rate);
+  console.log(wallet);
+
   return deployer.then(async () => {
     // deploy token
-    return deployer.deploy(KratosToken, tokenTotalSupply);
+    return deployer.deploy(KratosToken, tokenTotalSupply, {gas: 4000000, gasPrice: web3.toWei(10, "gwei")} );
   }).then(async () => {
+    console.log(KratosToken.address);
+
     // deploy crowdsale contract with initialization parameters
     return deployer.deploy(
       KratosPresale,
@@ -25,7 +34,7 @@ module.exports = async (deployer) => {
       closingTime,
       rate,
       wallet,
-      KratosToken.address, 
+      KratosToken.address
     );
   }).then(async () => {
 
