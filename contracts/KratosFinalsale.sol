65419beec036eed3382cb6ef35ef4f6585a72cb2
Sue Kah Yong <skybach@gmail.com>
2018-05-30 12:22:14 +0800

    add trader

---
+++ contracts/KratosFinalsale.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.18;
+
+import "openzeppelin-solidity/contracts/crowdsale/distribution/PostDeliveryCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/WhitelistedCrowdsale.sol";
+
+import "./KratosToken.sol";
+
+contract KratosFinalsale is CappedCrowdsale, WhitelistedCrowdsale, PostDeliveryCrowdsale {
+
+    function KratosFinalsale(
+        uint256 _cap,
+        uint256 _openingTime,
+        uint256 _closingTime,
+        uint256 _rate,
+        address _wallet,
+        KratosToken _token
+    ) public
+        Crowdsale(_rate, _wallet, _token)
+        CappedCrowdsale(_cap) // hard cap
+        TimedCrowdsale(_openingTime, _closingTime) {
+    }
+
+    function setRate(uint256 _rate) external onlyOwner onlyWhileOpen {
+        // solium-disable-next-line security/no-block-members
+        require(_rate > 0);
+
+        rate = _rate;
+    }
+
+    function setClosingTime(uint256 _closingTime) external onlyOwner {
+        // solium-disable-next-line security/no-block-members
+        require(_closingTime >= block.timestamp);
+        require(_closingTime >= openingTime);
+
+        closingTime = _closingTime;
+    }
+
+    function withdrawTokensMultiple(address[] addresses) public onlyOwner {
+        require(hasClosed());
+        for (uint i = 0; i < addresses.length; i++) {
+            address addr = addresses[i];
+            uint256 amount = balances[addr];
+            if (amount > 0) {
+                balances[addr] = 0;
+                _deliverTokens(addr, amount);
+            }
+        }
+    }
+}
